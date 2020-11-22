aa3a151d9b5acfa761e59e90cedf5daec91a9f93
SvyatoslavUA <72656919+SvyatoslavUA@users.noreply.github.com>
2020-11-22 22:19:08 +0200

    Add files via upload

--- Laba6.3/Laba6.3/Source.cpp
+++ Laba6.3/Laba6.3/Source.cpp
@@ -6,6 +6,14 @@ using namespace std;
 template < typename T >
 void Print(T* B, const int size)
 {
+    cout << "template" << endl;
+    for (int i = 0; i < size; i++)
+        cout << setw(5) << B[i];
+    cout << endl;
+}
+void Print(int* B, const int size)
+{
+    cout << "not template" << endl;
     for (int i = 0; i < size; i++)
         cout << setw(5) << B[i];
     cout << endl;
@@ -26,18 +34,26 @@ int main()
 {
     const int i = 5;
     int B[i];
+ 
+    string C[] = { "f", "a", "t", "h" };
     for (int i = 0; i < 5; i++)
 
     {
+        cout << endl;
         cout << "B [" << i << "]= ";
         cin >> B[i];
     }
     {
-        cout << "Original masive" << endl;
-        Print<int>(B, i);
+        cout << endl;
+        cout << "Original array" << endl;
+        Print<int>(B, i); // test template function for int 
+        Print<string>(C, 4); // test template function for string 
+        Print(B, i); // test not template function for int 
+        Print(C, 4); // test template function for string - we call template function because not template for string is not found
         cout << endl;
     }
     {
+        cout << endl;
       cout << "B = " << Suma(B) << endl;
     }
     return 0;
